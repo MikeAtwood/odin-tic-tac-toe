2ffc2e7d658ded0c32358ce91e807b8b5f18b716
Deadpool1 <atwood169@yahoo.com>
2023-04-02 12:57:00 -0500

    created makeMove, checkForWinner and refactored

--- script.js
+++ script.js
@@ -9,36 +9,60 @@ const gameBoardModule = (function () {
     const player2 = createPlayer("PlayerTwo", "O", false, false)
     console.log(player1, player2)
 
-    let gameBoard = []
+    let gameBoard = ['', '', '', '', '', '', '', '', '']
 
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+        [0, 4, 8], [2, 4, 6] // diagonal
+    ];
 
-// Function for making a move
-const playerTurn = (function () {
-    const box = document.querySelectorAll(".cell")
-    box.forEach(box => {
-        box.addEventListener('click', e => {
-            if (player1.turn === true && e.target.textContent == '') {
-                gameBoard[e.target.id] = player1.symbol;
-                box.textContent = player1.symbol;
-                box.style.color = '#EE6C4D'
-                player1.turn = false;
-                player2.turn = true;
-            } else if (player2.turn == true && e.target.textContent == '' && player2.ai == false) {
-                gameBoard[e.target.id] = player2.symbol;
-                box.textContent = player2.symbol;
-                box.style.color = '#98C1D9'
-                player1.turn = true;
-                player2.turn = false;
-
-                console.log(gameBoard)
-            } else {
-                return;
+    function checkForWinner(symbol) {
+        for (let i = 0; i < winningCombos.length; i++) {
+            const [a, b, c] = winningCombos[i];
+            if (gameBoard[a] === symbol && gameBoard[b] === symbol && gameBoard[c] === symbol) {
+                return true
             }
-        });
-    })
+        }
+        return false
+    }
 
-})();
-    return { playerTurn }
+    function makeMove(index, symbol) {
+        gameBoard[index] = symbol;
+    }
+
+
+    // Function for making a move
+    const playerTurn = (function () {
+        const box = document.querySelectorAll(".cell")
+        box.forEach(box => {
+            box.addEventListener('click', e => {
+                if (player1.turn === true && e.target.textContent == '') {
+                    makeMove(e.target.id, player1.symbol);
+                    box.textContent = player1.symbol;
+                    box.style.color = '#EE6C4D'
+                    player1.turn = false;
+                    player2.turn = true;
+                    if (checkForWinner(player1.symbol)) {
+                        console.log(player1.name + " wins!")
+                    }
+                } else if (player2.turn == true && e.target.textContent == '' && player2.ai == false) {
+                    makeMove(e.target.id, player2.symbol);
+                    box.textContent = player2.symbol;
+                    box.style.color = '#98C1D9'
+                    player1.turn = true;
+                    player2.turn = false;
+                    if (checkForWinner(player2.symbol)) {
+                        console.log(player2.name + " wins!")
+                    }
+                } else {
+                    return;
+                }
+            });
+        })
+
+    })();
+    return { makeMove, checkForWinner, playerTurn, player1, player2 }
 })();
 
 const renderArrayToScreen = (() => {
